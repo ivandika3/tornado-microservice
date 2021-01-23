d4c254f05b645e88d0ef3a5d040d3b4dabc082e6
Ivan Andika <ivanandika@u.nus.edu>
2021-01-23 18:13:13 +0800

    Implemented memcached to cache user info

--- services/public-api/public-api.py
+++ services/public-api/public-api.py
@@ -4,6 +4,8 @@ import tornado.log
 import tornado.options
 from tornado.httpclient import AsyncHTTPClient
 from tornado.httputil import url_concat
+from pymemcache.client import base
+from pymemcache import serde
 import logging
 import json
 import urllib
@@ -23,70 +25,59 @@ class BaseHandler(tornado.web.RequestHandler):
 
 class ListingsHandler(BaseHandler):
     @tornado.gen.coroutine
+    def get_user(self, user_id, http_client, memcache_client):
+        user = memcache_client.get(str(user_id))
+        if user is None:
+            print("cache miss")
+            userURL = USERS_URL + "/" + str(user_id)
+            usersResp = yield http_client.fetch(userURL, raise_error=False)
+            userJSON = json.loads(usersResp.body.decode('utf-8'))
+            if not userJSON['result']:
+                http_client.close()
+                self.write_json(userJSON, status_code=400)
+                return
+            user = userJSON['user']
+            memcache_client.set(str(user_id), user)
+        else:
+            print("cache hit")
+        return user
+
+    @tornado.gen.coroutine
     def get(self):
         # Parsing pagination params
         page_num = self.get_argument("page_num", 1)
         page_size = self.get_argument("page_size", 10)
         user_id = self.get_argument("user_id", None)
 
-        # There are two approaches that I can think of to join Listings and Users based on the user_id
-        # 1. For every listing, call an API to fetch the user with the specific user_id
-        #       Limitation: need to call an API multiple times
-        # 2. Use cache to store the user information so that we don't have to fetch the user for each listing
-        #       Limitation: memory overhead (public-api service might be bloated) 
-        # Decided to use approach 1
-
+       
         http_client = AsyncHTTPClient()
+        # Use memcached to cache repeated user 
+        memcache_client = base.Client('localhost', serde=serde.pickle_serde)
         try :
+            listingParams = {}
             if user_id is not None:
-                # Preparing URL parameters
                 listingParams = {"user_id": user_id, "page_num": page_num, "page_size": page_size}
-                listingsURL = url_concat(LISTINGS_URL, listingParams)
                 userURL = USERS_URL + "/" + str(user_id)
-                listingsResp, usersResp = yield [http_client.fetch(listingsURL, raise_error=False), http_client.fetch(userURL, raise_error=False)]
-
-                listingsJSON = json.loads(listingsResp.body.decode('utf-8'))
-                if not listingsJSON['result']:
-                    http_client.close()
-                    self.write_json(listingsJSON, status_code=400)
-                    return
-
-                userJSON = json.loads(usersResp.body.decode('utf-8'))
-                if not userJSON['result']:
-                    http_client.close()
-                    self.write_json(userJSON, status_code=400)
-                    return
-                
-                listings = listingsJSON['listings']
-                user = userJSON['user'] 
-
-                for listing in listings:
-                    listing['user'] = user
             else:
                 listingsParams = {"page_num": page_num, "page_size": page_size}
-                listingsURL = url_concat(LISTINGS_URL, listingsParams)
-                listingsResp = yield http_client.fetch(listingsURL, raise_error=False)
-                listingsJSON = json.loads(listingsResp.body.decode('utf-8'))
 
-                if not listingsJSON['result']:
-                    http_client.close()
-                    self.write_json(listingsJSON)
-                    return
-                listings = listingsJSON['listings']
+            listingsURL = url_concat(LISTINGS_URL, listingParams)
+            listingsResp = yield http_client.fetch(listingsURL, raise_error=False)
+            listingsJSON = json.loads(listingsResp.body.decode('utf-8'))
+            if not listingsJSON['result']:
+                http_client.close()
+                self.write_json(listingsJSON, status_code=400)
+                return
+            listings = listingsJSON['listings']
 
+            if user_id is not None: 
+                user = yield self.get_user(user_id, http_client, memcache_client)
+            else :
                 for listing in listings:
-                    usersURL = USERS_URL + "/" + str(listing['user_id'])
-                    userResp = yield http_client.fetch(USERS_URL + "/" + str(listing['user_id']), raise_error=False)
-
-                    userJSON = json.loads(userResp.body.decode('utf-8'))
-                    if not userJSON['result']:
-                        http_client.close()
-                        self.write_json(userJSON, status_code=400)
-                        return
-
-                    user = userJSON['user']
+                    user = yield self.get_user(listing['user_id'], http_client, memcache_client)
                     listing['user'] = user
         except Exception as e:
+            print(e)
             self.write_json({"result": False, "errors": str(e)}, status_code=400)
             http_client.close()
             return
